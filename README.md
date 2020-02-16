677be37ead050fe814974649d91e095fa65c01f2
Mark Snow <highspeedsnow@gmail.com>
2020-02-16 10:58:15 -0800

    added html div functionality to image2html-css-md

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Cisco Tetration - Hands On Lab
   
-[Documentation](https://onstakinc.github.io/cisco-tetration-hol/labguide/)
+[Lab Guide Documentation](https://onstakinc.github.io/cisco-tetration-hol/labguide/)
 
 This lab is provided using the [GPL-3.0](https://github.com/OnstakInc/cisco-tetration-hol/blob/master/LICENSE). You may fork this repo and use it mostly as you like, however you must retain the same GPL license, and you must give credit. This lab was created by [Raheel Anwar](https://github.com/raheel-anwar), Muneeb Ali, [Matt Mullen](https://github.com/mamullen13316), and [Mark Snow](https://github.com/highspeedsnow) with [OnStak](https://onstak.com). 
--- labguide/image2html-css-md.py
+++ labguide/image2html-css-md.py
@@ -4,6 +4,7 @@ from shutil import copy2
 
 module = Path(os.getcwd()).parent.name
 url_dict = {}
+stepnum_dict = {}
 
 files = [file for file in glob.glob('*.png')]
 files.sort(key=os.path.getmtime)
@@ -12,6 +13,8 @@ for i,file in enumerate(files):
     new_fn = f'{module}_' + str(i).zfill(3) + '.png'
     os.rename(file,new_fn)
     url_dict[i] = f'https://onstakinc.github.io/cisco-tetration-hol/labguide/{module}/images/{new_fn}'
+    stepnum = str(i).zfill(3)
+    stepnum_dict[i] = f'{stepnum}'
 
 os.chdir('..')
 
@@ -28,9 +31,11 @@ with open('README.md','w') as f:
     f.write('  \n')
 
     for key in sorted(url_dict.keys()):
+        f.write(f'<div class="step" id="step-{stepnum_dict[key]}">')
+        f.write(f'<a href="#step-{stepnum_dict[key]}">Step {stepnum_dict[key]}</a>')
+        f.write(f'</div>')
         f.write(f'<a href="{url_dict[key]}"><img src="{url_dict[key]}" style="width:100%;height:100%;"></a>  \n')
         f.write('  \n')
 
     f.write('  \n')
     f.write('[Return to Table of Contents](https://onstakinc.github.io/cisco-tetration-hol/labguide/)')
-
