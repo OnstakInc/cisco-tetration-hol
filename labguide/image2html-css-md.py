d87feb69037a63a29261ec750adaa2168368c2dc
Mark Snow <highspeedsnow@gmail.com>
2020-02-15 20:42:59 -0800

    image2html-css-md

---
+++ labguide/image2html-css-md.py
@@ -0,0 +1,31 @@
+import os, glob
+from pathlib import Path
+from shutil import copy2
+
+module = Path(os.getcwd()).parent.name
+url_dict = {}
+
+files = [file for file in glob.glob('*.png')]
+files.sort(key=os.path.getmtime)
+
+for i,file in enumerate(files):
+    new_fn = f'{module}_' + str(i).zfill(3) + '.png'
+    os.rename(file,new_fn)
+    url_dict[i] = f'https://onstakinc.github.io/cisco-tetration-hol/labguide/{module}/images/{new_fn}'
+
+os.chdir('..')
+copy2('README.md', 'README.bak.md') 
+
+with open('README.md','w') as f:
+    f.write('# Cisco Tetration - Hands-On Lab\n')
+    f.write('  \n')
+    f.write(f'## {module.capitalize()}\n')
+    f.write('  \n')
+
+    for key in sorted(url_dict.keys()):
+        f.write(f'<img src="{url_dict[key]}" style="width:100%;height:100%;">  \n')
+        f.write('  \n')
+
+    f.write('  \n')
+    f.write('[Return to Table of Contents](https://onstakinc.github.io/cisco-tetration-hol/labguide/)')
+
